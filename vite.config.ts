a87d44045643fabdc745c005a02dddb7c73e576c
moazahmad <moaz.ahmad@royalcyber.com>
2025-03-04 03:22:40 +0500

    new

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   // Set the base path to your repository name for GitHub Pages
-  base: '/CryptoWebsite', // Replace 'your-repo-name' with the actual repo name
+  base: "", // Replace 'your-repo-name' with the actual repo name
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
