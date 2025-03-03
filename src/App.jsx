fc8702b61b18082cfed1acc53e7049035e04c74f
moazahmad <moaz.ahmad@royalcyber.com>
2025-03-04 03:42:03 +0500

    hi ther

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import LandingPage from './pages/LandingPage';
@@ -8,19 +8,17 @@ import CoinDetailsPage from './pages/CoinDetailsPage';
 
 function App() {
   return (
-    <Router>
-      <div className="min-h-screen bg-gray-50 flex flex-col">
-        <Navbar />
-        <main className="flex-grow">
-          <Routes>
-            <Route path="/" element={<LandingPage />} />
-            <Route path="/news" element={<NewsPage />} />
-            <Route path="/coin/:id" element={<CoinDetailsPage />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </Router>
+    <div className="min-h-screen bg-gray-50 flex flex-col">
+      <Navbar />
+      <main className="flex-grow">
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/news" element={<NewsPage />} />
+          <Route path="/coin/:id" element={<CoinDetailsPage />} />
+        </Routes>
+      </main>
+      <Footer />
+    </div>
   );
 }
 
