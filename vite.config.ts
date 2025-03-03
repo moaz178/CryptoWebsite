11c20edbf1477e30dd3ecab561534e4c9e69a6fc
moazahmad <moaz.ahmad@royalcyber.com>
2025-03-04 03:33:15 +0500

    hello

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,12 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-
-  base: "/CryptoWebsite/", 
+  // Ensure the correct base path with a trailing slash
+  base: '/CryptoWebsite/',
   plugins: [react()],
+  build: {
+    outDir: 'docs', // If you are using GitHub Pages with "docs" folder
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
